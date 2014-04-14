5971de8f6730c40c71fa5eefeeeb787ad83fc759
alex <a>
2014-04-14 10:35:53 +0200

    first cut

--- app/src/main/java/com/augnition/scan_for_glass/test_int_s4/app/ScanActivity.java
+++ app/src/main/java/com/augnition/scan_for_glass/test_int_s4/app/ScanActivity.java
@@ -1,23 +1,43 @@
 package com.augnition.scan_for_glass.test_int_s4.app;
 
+import android.IntentIntegrator;
+import android.IntentResult;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class ScanActivity extends Activity {
 
+    private static final String TAG = ScanActivity.class.getSimpleName();
+
+    ////////////////////////////////////////
+    // ScanActivity
+    ////////////////////////////////////////
+
+    private void startScan() {
+        IntentIntegrator ii = new IntentIntegrator(this);
+        ii.initiateScan();
+    }
+
+    ////////////////////////////////////////
+    // Activity
+    ////////////////////////////////////////
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
-    }
 
+        startScan();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        
+
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.scan, menu);
         return true;
@@ -35,4 +55,20 @@ public class ScanActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (scanResult != null) {
+        // handle scan result
+            String str = scanResult.toString();
+            Log.d(TAG, str);
+        }
+        else {
+            // else continue with any other code you need in the method
+            Log.d(TAG, "oops");
+
+    }
+}
 }
